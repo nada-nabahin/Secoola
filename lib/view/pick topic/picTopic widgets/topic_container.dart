da5263b61c9b2de9e3d67653d05743159a50aee3
Nada <nnada.ali2021@gmail.com>
2023-08-14 21:13:59 +0300

    Secoola

--- lib/view/pick topic/picTopic widgets/topic_container.dart
+++ lib/view/pick topic/picTopic widgets/topic_container.dart
@@ -1,40 +1,77 @@
 import 'package:flutter/material.dart';
 
-class TopicContainer extends StatelessWidget {
-  String imageLink;
-  String topicName;
-  TopicContainer({super.key, required this.imageLink, required this.topicName});
+class TopicContainer extends StatefulWidget {
+  final String imageLink;
+  final String topicName;
+
+  const TopicContainer({
+    required this.imageLink,
+    required this.topicName,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _TopicContainerState createState() => _TopicContainerState();
+}
+
+class _TopicContainerState extends State<TopicContainer> {
+  bool isSelected = false;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(17),
-            color: Colors.white,
-          ),
-          child: Center(
-            child: Image(
-              image: AssetImage(imageLink),
-              width: 24,
-              height: 24,
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          isSelected = !isSelected;
+        });
+      },
+      child: Column(
+        children: [
+          Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.white,
+              border: isSelected
+                  ? Border.all(color: Color(0xff00A9B7), width: 2)
+                  : null, // No border when not selected
+            ),
+            child: Stack(
+              alignment: Alignment.topRight,
+              children: [
+                Center(
+                  child: Image(
+                    image: AssetImage(widget.imageLink),
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
+                if (isSelected)
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xff00A9B7),
+                    ),
+                    child: Icon(Icons.check, color: Colors.white, size: 16),
+                  ),
+              ],
             ),
           ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        Text(
-          topicName,
-          style: TextStyle(
-            fontSize: 14,
-            color: Color(0xffA9AEB2),
+          const SizedBox(
+            height: 10,
           ),
-        ),
-      ],
+          Text(
+            widget.topicName,
+            style: TextStyle(
+              fontSize: 14,
+              color: isSelected ? Color(0xff00A9B7) : Color(0xffA9AEB2),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/view/pick topic/pick_topic.dart
+++ lib/view/pick topic/pick_topic.dart
@@ -18,105 +18,103 @@ class _PickTopicState extends State<PickTopic> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xffFAFAFA),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Pick your favorite topic',
-                style: TextStyle(fontSize: 24, color: Colors.black),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pick your favorite topic',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: 294,
+              height: 41,
+              child: const Text(
+                textAlign: TextAlign.center,
+                'Choose your favorite topic to help us deliver the most suitable course for you.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xffA9AEB2),
+                ),
               ),
-              const SizedBox(
-                height: 15,
-              ),
-              Container(
-                width: 294,
-                height: 41,
-                child: const Text(
-                  textAlign: TextAlign.center,
-                  'Choose your favorite topic to help us deliver the most suitable course for you.',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xffA9AEB2),
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              width: 335,
+              height: 386,
+              child: GridView.count(
+                scrollDirection: Axis.vertical,
+                mainAxisSpacing: 20,
+                crossAxisCount: 3,
+                children: const [
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/palette.png',
+                    topicName: 'Art',
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Container(
-                width: 335,
-                height: 386,
-                child: GridView.count(
-                  scrollDirection: Axis.vertical,
-                  crossAxisCount: 3,
-                  children: [
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/palette.png',
-                      topicName: 'Art',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/business.png',
-                      topicName: 'business',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/noodles.png',
-                      topicName: 'Culinary',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/desktop.png',
-                      topicName: 'Coding',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/fountain-pen.png',
-                      topicName: 'Design',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/game-controller.png',
-                      topicName: 'Gaming',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/mental-health.png',
-                      topicName: 'Lifestyle',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/music.png',
-                      topicName: 'Music',
-                    ),
-                    TopicContainer(
-                      imageLink: 'assets/iconImages/football.png',
-                      topicName: 'Sport',
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              CommonButton(
-                buttonLabel: 'Start your journey',
-                onPressed: () {
-                  Get.to(HomeScreen());
-                },
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Container(
-                width: 294,
-                height: 41,
-                child: const Text(
-                  'You can still change your topic again later',
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Color(0xffA9AEB2),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/business.png',
+                    topicName: 'business',
                   ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/noodles.png',
+                    topicName: 'Culinary',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/desktop.png',
+                    topicName: 'Coding',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/fountain-pen.png',
+                    topicName: 'Design',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/game-controller.png',
+                    topicName: 'Gaming',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/mental-health.png',
+                    topicName: 'Lifestyle',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/music.png',
+                    topicName: 'Music',
+                  ),
+                  TopicContainer(
+                    imageLink: 'assets/iconImages/football.png',
+                    topicName: 'Sport',
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            CommonButton(
+              buttonLabel: 'Start your journey',
+              onPressed: () {
+                Get.to(const HomeScreen());
+              },
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 294,
+              height: 41,
+              child: const Text(
+                'You can still change your topic again later',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Color(0xffA9AEB2),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
